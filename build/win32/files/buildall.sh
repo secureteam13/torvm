f1b3a76ce499e8526358686e9f16eca49c2eea5a
coderman <coderman@55e972cd-5a19-0410-ae62-a4d7a52db4cd>
2008-09-23 06:27:25 +0000

    Win32 kernel driver build integration of Tap-Win32 and WinPcap parts, other misc clearing of commit code jams.
    git-svn-id: https://svn.torproject.org/svn/torvm/trunk@16934 55e972cd-5a19-0410-ae62-a4d7a52db4cd

--- build/win32/files/buildall.sh
+++ build/win32/files/buildall.sh
@@ -2,10 +2,31 @@
 export PATH=.:/usr/local/bin:/usr/bin:/bin:/mingw/bin:/c/WINDOWS/system32:/c/WINDOWS:/c/WINDOWS/System32/Wbem
 export LD_LIBRARY_PATH=/usr/local/lib:/usr/lib:/lib:/mingw/lib
 export ddir=/c/Tor_VM
-mkdir $ddir
+export libdir="${ddir}/lib"
+export bindir="${ddir}/bin"
+export confdir="${ddir}/config"
+export statedir="${ddir}/state"
 
-cp /usr/bin/msys-z.dll $ddir/
-cp /usr/bin/msys-1.0.dll $ddir/
+foreach dir in $ddir $libdir $bindir $confdir $statedir; do
+  if [ ! -d $dir ]; then
+    mkdir $dir
+  fi
+done
+
+if [ -d /usr/usr ]; then
+  # ahh, gotta love the msys /usr <-> / equivalence hack...
+  cd /usr/usr
+  mv local ../
+  rmdir usr
+fi
+cp /usr/local/bin/aclocal-* /bin/aclocal
+cp /usr/local/bin/autoconf-* /bin/autoconf
+cp /usr/local/bin/autoheader-* /bin/autoheader
+cp /usr/local/bin/automake-* /bin/automake
+
+cp /bin/msys-z.dll $libdir/
+cp /bin/msys-1.0.dll $libdir/
+
 
 echo "Building pthreads-w32 ..."
 cd /usr/src
@@ -17,7 +38,7 @@ if (( $? != 0 )); then
   echo "ERROR: pthreads-32 build failed." >&2
   exit 1
 fi
-cp pthreadGC2.dll $ddir/
+cp pthreadGC2.dll $libdir/
 
 
 echo "Building SDL library ..."
@@ -36,19 +57,11 @@ if (( $? != 0 )); then
   exit 1
 fi
 make install
-cp /usr/bin/SDL.dll $ddir/
-
-
-echo "Extracting WinPcap runtime files ..."
-cd /usr/src
-tar zxvf WinPcap-4.1-files.tar.gz
-
-echo "Extracting WinPcap developer files ..."
-cd /usr/src
-tar zxvf WpdPack_4_1_beta4.tar.gz
+cp /usr/bin/SDL.dll $libdir/
 
 
 echo "Building zlib ..."
+cd /usr/src
 tar zxvf zlib-1.2.3.tar.gz
 cd zlib-1.2.3
 ./configure --prefix=/usr
@@ -62,6 +75,91 @@ if (( $? != 0 )); then
   exit 1
 fi
 make install
+
+
+echo "Locating Windows Driver Development Kit ..."
+# there has to be a better way to do this .... ugg
+found=0
+DDKNAME="WINDDK"
+DDKDIR=""
+DDKENV=""
+DDKVER=""
+DDKMAJORVER=""
+checkdirs="C D E F G H I J K L M N O P Q R S T U V W X Y Z"
+for cdir in $checkdirs; do
+  if (( $found == 0 )); then
+    if [ -d /$cdir/ ]; then
+      tddkdir="/$cdir/$DDKNAME"
+      if [ ! -d $tddkdir ]; then
+        tddkdir=`find /$cdir/ -type d -name $DDKNAME 2>/dev/null`
+      fi
+      if [ -d $tddkdir ]; then
+        envf=`find $tddkdir -type f -name setenv.bat 2>/dev/null`
+	if [ -f $envf ]; then
+	  ddkbase=`dirname $envf`
+	  ddkbase=`dirname $ddkbase`
+          wddkbase=`echo $ddkbase | sed 's/^...//' | sed 's/\//\\\/g'`
+	  DDKDIR="${cdir}:\\${wddkbase}"
+	  DDKENV="${DDKDIR}\bin\setenv.bat"
+	  DDKVER=`grep '^Build' "${ddkbase}/Uninstall/Uninstall.ini" | sed 's/.*=//'`
+	  DDKMAJORVER=`echo $DDKVER | sed 's/\..*//'`
+          echo "Found DDK install at $DDKDIR , using env script $DDKENV"
+	  cp "${$ddkbase}/tools/devcon/i386/devcon.exe" $bindir
+	  found=1
+	fi
+      fi
+    fi
+  fi
+done
+if (( $found == 0 )); then
+  echo "ERROR: Unable to locate the Windows Driver Development Kit." >&2
+  echo "       Please provide the correct location to configure or"  >&2
+  echo "       install the 1830_usa_ddk.iso software if needed." >&2
+  exit 1
+fi
+export DDKDIR
+export DDKENV
+export DDKVER
+export DDKMAJORVER
+
+
+echo "Building openvpn tap-win32 driver ..."
+cd /usr/src
+tar zxvf openvpn-2.1_rc10.tar.gz
+cd openvpn-2.1_rc10
+patch -p1 < ../openvpn-tor-tap-win32-driver.patch 2>/dev/null
+aclocal && autoheader && autoconf && automake --add-missing --copy
+if (( $? != 0 )); then
+  echo "ERROR: openvpn autotools update failed." >&2
+  exit 1
+fi
+MAN2HTML=/bin/true.exe ./configure --prefix=/usr \
+ --with-cygwin-native \
+ --disable-debug \
+ --disable-lzo \
+ --disable-ssl \
+ --disable-crypto 
+if (( $? != 0 )); then
+  echo "ERROR: openvpn configure failed." >&2
+  exit 1
+fi
+cd tap-win32
+echo "call $DDKENV $DDKDIR fre WXP" > dobuild.bat
+echo "build -cef" >> dobuild.bat
+echo "exit 0" >> dobuild.bat
+cmd.exe /k dobuild.bat
+if (( $? != 0 )); then
+  echo "ERROR: openvpn tap-win32 build failed." >&2
+  exit 1
+fi
+
+
+echo "Building WinPcap ..."
+cd /usr/src
+tar zxvf WpcapSrc_4_1_beta4.tar.gz
+cd WpcapSrc_4_1_beta4
+patch -p1 < ../winpcap-tor-device-mods.patch 2>/dev/null
+echo "KILL ME";sleep 3600
 
 
 echo "Building qemu ..."
@@ -91,15 +189,12 @@ if (( $? != 0 )); then
   echo "ERROR: qemu build failed." >&2
   exit 1
 fi
-cp i386-softmmu/qemu.exe $ddir/
-cp pc-bios/bios.bin $ddir/
-cp pc-bios/vgabios.bin $ddir/
-cp pc-bios/vgabios-cirrus.bin $ddir/
-cp /usr/src/add/* $ddir/
-
-# still need to handle WinPcap installation; perhaps include link to 4.1 installer exe
-# or wget it at runtime if not present.
-# cp /usr/src/WinPcap-4.1-files/* $ddir/
+cp i386-softmmu/qemu.exe $bindir/
+cp pc-bios/bios.bin $bindir/
+cp pc-bios/vgabios.bin $bindir/
+cp pc-bios/vgabios-cirrus.bin $bindir/
+cp /usr/src/add/* $libdir/
+#cp /usr/src/WinPcap-4.1-files/* $ddir/
 
 echo "DONE."
 exit 0
