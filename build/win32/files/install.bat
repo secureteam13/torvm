9dd17253709d05fdc0379ff94e67119562ea5c2f
coderman <coderman@55e972cd-5a19-0410-ae62-a4d7a52db4cd>
2008-09-24 02:54:52 +0000

    Escape dir separators in mingw/msys profile script so shell is happy again.
    git-svn-id: https://svn.torproject.org/svn/torvm/trunk@16952 55e972cd-5a19-0410-ae62-a4d7a52db4cd

--- build/win32/files/install.bat
+++ build/win32/files/install.bat
@@ -55,7 +55,7 @@ cd /d %DDRV%
 cd %MDIR%\%MVER%
 set PATH=%DDRV%%MDIR%\%MVER%\bin;%DDRV%%MDIR%\%MVER%;%PATH%
 set BUILDER=/usr/src/buildall.sh
-set MSYSROOT=C:\msys\1.0\bin\
+set MSYSROOT=C:\\msys\\1.0
 md etc\profile.d
 ECHO export MSYSROOT="%MSYSROOT%" > etc\profile.d\defpaths.sh
 set WD=C:\msys\1.0\bin\
